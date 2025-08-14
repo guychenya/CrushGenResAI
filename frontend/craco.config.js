dcae9fab0855a843aa1aad62c38d6426ac8dbb50
guychenya <guy.chenya@gmail.com>
2025-08-14 12:09:57 +0300

    fix: Add fs fallback to craco config

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -8,6 +8,7 @@ module.exports = {
         "crypto": require.resolve("crypto-browserify"),
         "stream": require.resolve("stream-browserify"),
         "buffer": require.resolve("buffer"),
+        "fs": false,
       };
       webpackConfig.plugins.push(
         new webpack.ProvidePlugin({
